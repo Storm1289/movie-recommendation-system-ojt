f4a224439bd8beeb83e4066342c926043995e558
Divakar <divakarsharma2934@gmail.com>
2026-04-07 02:32:04 +0530

    aa

--- api/index.py
+++ api/index.py
@@ -7,4 +7,6 @@ BACKEND_DIR = ROOT_DIR / "backend"
 if str(BACKEND_DIR) not in sys.path:
     sys.path.insert(0, str(BACKEND_DIR))
 
-from main import app
+from main import app as backend_app
+
+app = backend_app
